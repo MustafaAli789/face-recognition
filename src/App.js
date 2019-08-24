a07e59eff1ccba2aa6298204a21cb9b15e4105cd
Mustafa Ali <alimustafa0987@gmail.com>
2019-08-23 22:31:02 -0400

    particle effects added and input form added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
+
+
 
 import './App.css';
 
 function App() {
+
+  const particlesOptions = {
+    particles: {
+        number: {
+          value: 80,
+          density: {
+            enable: true,
+            value_area: 800
+          }
+        }
+    }
+  }
+
   return (
     <div className="App">
+      <Particles className="particles"
+        params={particlesOptions}
+      />
       <Navigation/>
       <Logo />
-      {/*<ImageLinkForm />*/}
+      <Rank/>
+      <ImageLinkForm />
     </div>
   );
 }
