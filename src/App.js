0278e6d3afe3c5372fcd6b74cdb277ca760dac45
Mustafa Ali <alimustafa0987@gmail.com>
2019-08-23 23:07:06 -0400

    added api to detect faces

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
 import Rank from './components/Rank/Rank.js';
-
-
-
+import Clarifai from 'clarifai';
 import './App.css';
 
-function App() {
 
-  const particlesOptions = {
+const app = new Clarifai.App({
+ apiKey: '459c1054278d4c6fa4fd9b5011de32b5',
+});
+
+const particlesOptions = {
     particles: {
         number: {
           value: 80,
@@ -23,17 +24,46 @@ function App() {
     }
   }
 
-  return (
-    <div className="App">
-      <Particles className="particles"
-        params={particlesOptions}
-      />
-      <Navigation/>
-      <Logo />
-      <Rank/>
-      <ImageLinkForm />
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state={
+      input: '',
+    }
+  }
+
+  onInputChange=(event)=>{
+    console.log(event.target.value);
+  }
+
+  onSubmit=()=>{
+    console.log("click");
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+      function(response) {
+        console.log(response)
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <Particles className="particles"
+          params={particlesOptions}
+        />
+        <Navigation/>
+        <Logo />
+        <Rank/>
+        <ImageLinkForm onInputChanged={this.onInputChange} onButtonSubmit={this.onSubmit}/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
